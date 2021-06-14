bed8706c5815843e814ccd40693d67df697df667
Oluwatobi Adebiyi <awedupe07@gmail.com>
2021-06-14 23:52:30 +0100

    initial client commit

---
+++ client/index.js
@@ -0,0 +1,29 @@
+const xmpp = require("simple-xmpp");
+
+xmpp.on("online", data => {
+  console.log("Hey you're on the XMPP server");
+  console.log(`Conected as ${data.jid.user}`)
+  sendMessageEvery(5000);
+});
+
+xmpp.on("error", error => {
+  console.log(`Something terrible happened: ${error}`);
+});
+
+xmpp.on("chat", (from, message) => {
+  console.log(`Got a message: ${message} from ${from}`);
+});
+
+
+function sendMessageEvery(interval) {
+  setInterval(() => {
+    xmpp.send("tobi@localhost", `Hi @ ${Date.now()}`)
+  }, interval);
+}
+
+xmpp.connect({
+  "jid": "admin@localhost",
+  "password": "password",
+  "host": "localhost",
+  "port": "5222"
+});
